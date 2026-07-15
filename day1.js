89f44c01dea682ee9066bc27753ca0c03fafacf2
NGR-Np <ngr112001@gmail.com>
2026-07-15 10:25:11 +0545

    day 1

---
+++ day1.js
@@ -0,0 +1,104 @@
+// node --watch day1.js
+
+// const greeting = "Hello, World!";
+// const age = 21
+
+
+// console.log(greeting);
+// console.log(age);
+
+
+
+// let greeting = "Hello, World!";
+// let age = 21;
+
+// console.log(age);
+
+// // after 1 year
+// age = 22;
+
+// console.log("type of greeting:", typeof greeting, " value=", greeting);
+// console.log("type of age:", typeof age, " value=", age);
+
+
+
+
+// let userInfo ={
+//     name: "John Doe",
+//     age: 21,
+//     email: "john.doe@example.com"
+// }
+// console.log("type of userInfo:", typeof userInfo, " value=", userInfo);
+
+
+// let zooAnimal = ["Lion", "Tiger", "Elephant"]
+// console.log("type of zooAnimal:", typeof zooAnimal, " value=", zooAnimal);
+// zooAnimal = ["Giraffe", "Zebra", "Monkey"]
+
+// console.log( zooAnimal);
+
+// const date = new Date();
+// console.log("type of date:", typeof date, " value=", date);
+
+// const isActive = true;
+
+
+
+
+
+
+
+// let userInfo ={
+//     name: "John Doe",
+//     age: 21,
+//     email: "john.doe@example.com"
+// }
+
+// const message = `Hello, my name is ${userInfo.name}`
+
+// console.log(message);
+
+
+
+// let num1=5
+// let num2="5"
+
+// console.log(num1 === num2)
+
+// const aviSay=true
+// const surajSay=true
+
+// if(aviSay || surajSay ){
+//     console.log("itahari")
+// }else{
+//     console.log("One of them is false")
+// }
+
+// console.log(!aviSay)
+
+
+
+// const isAdult=true
+
+// if(bouncerSay && isAdult){
+    //     console.log("You can enter the club")
+    // }else{
+        //     console.log("You cannot enter the club")
+        // }
+        
+// const bouncerSay=true
+// const age=21
+// if(age>=21){
+//     console.log("You can enter the club")
+// }else{
+//     console.log("You cannot enter the club")
+// }
+// const hasSomethingInMyPocket=true
+// if(age >= 21 && !hasSomethingInMyPocket){
+//     console.log("I don't have anything in my pocket")
+//     console.log("You can enter the club")
+
+// }else{
+//     console.log("I have something in my pocket")
+//     console.log("You cannot enter the club")
+// }
