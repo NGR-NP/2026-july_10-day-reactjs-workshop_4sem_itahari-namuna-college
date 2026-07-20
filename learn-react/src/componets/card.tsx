ba9a6d87e8f7ea3b9bf6b9887836d00845cc1c55
NGR-Np <ngr112001@gmail.com>
2026-07-20 09:36:26 +0545

    day 4: code spliting step 1

--- learn-react/src/componets/card.tsx
+++ learn-react/src/componets/card.tsx
@@ -1,11 +1,9 @@
-type Props = {title: string, description: string}
+type Props = { title: string, description: string, img: string }
 
-export default function Card({title, description}: Props) {
-    return(
+export default function Card({ title, description, img }: Props) {
+    return (
         <div className="card">
-            <div className="card-header">
-                <h3>{title}</h3>
-            </div>
+            <CardHeader title={title} img={img} />
             <div className="card-body">
                 <p>{description}</p>
             </div>
@@ -14,3 +12,12 @@ export default function Card({title, description}: Props) {
 }
 
 
+// export function CardHeader({ title, img }: Omit<Props, "description">) { // Omit: remove description from Props type
+export function CardHeader({ title, img }: Pick<Props, "img" | "title">) { // Pick: only pick img and title from Props type
+    return (
+        <div className="card-header">
+            <img src={img} alt={title} />
+            <h3>{title}</h3>
+        </div>
+    )
+}
--- learn-react/src/pages/products.tsx
+++ learn-react/src/pages/products.tsx
@@ -1,22 +1,30 @@
 import Card from "../componets/card"
 
-const DataProducts=[
+const DataProducts = [
     {
+        img: "/hello/qrcode_github.com.png",
         title: "Product 1",
-        description: "This is the description for Product 1."
+        description: "This is the description for Product 1.",
+        
+        
     },
     {
+        img: "/hello/qrcode_github.com.png",
+
         title: "Product 2",
         description: "This is the description for Product 2."
     },
     {
+        img: "/hello/qrcode_github.com.png",
+
         title: "Product 3",
         description: "This is the description for Product 3."
     },
-     {
+    {
+        img: "/hello/qrcode_github.com.png",
         title: "Product 4",
         description: "This is the description for Product 4."
-    }   
+    }
 ]
 
 export default function ProductsPage() {
@@ -25,19 +33,21 @@ export default function ProductsPage() {
             <div>
                 <h1>Products Page</h1>
             </div>
-            {/* <Card title={DataProducts.at(0)?.title!} description={DataProducts.at(0)?.description!}/>
-            <Card title={DataProducts.at(1)?.title!} description={DataProducts.at(1)?.description!}/>
-            <Card title={DataProducts.at(2)?.title!} description={DataProducts.at(2)?.description!}/> */}
-
-
-
-
-
-            {
-                DataProducts.map((data, idx)=> (
-                    <Card key={idx} title={data.title} description={data.description}/>
-                ))
-            }
+            <div>
+                {/* <Card title={DataProducts.at(0)?.title!} description={DataProducts.at(0)?.description!}/>
+                
+                <Card title={DataProducts.at(1)?.title!} description={DataProducts.at(1)?.description!}/>
+                <Card title={DataProducts.at(2)?.title!} description={DataProducts.at(2)?.description!}/> */}
+            </div>
+        {
+          DataProducts.map((data, idx) =>{
+              return(
+                   <Card key={idx} title={data.title}  description={data.description} 
+                   img={data.img} />
+                  )}
+                
+          )
+        }
         </main>
     )
 }
