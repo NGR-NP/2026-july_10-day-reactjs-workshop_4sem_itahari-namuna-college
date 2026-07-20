c0f5016b8cf2eef6895d73559fc29d2907ad3497
NGR-Np <ngr112001@gmail.com>
2026-07-20 09:51:24 +0545

    day 4: code spliting step 2

--- learn-react/src/componets/card.tsx
+++ learn-react/src/componets/card.tsx
@@ -4,16 +4,19 @@ export default function Card({ title, description, img }: Props) {
     return (
         <div className="card">
             <CardHeader title={title} img={img} />
-            <div className="card-body">
-                <p>{description}</p>
-            </div>
+            <CardBody description={description} />
+        </div>
+    )
+}
+export function CardBody({ description }: Pick<Props, "description">) { // Pick: only pick description from Props type
+    return (
+        <div className="card-body">
+            <p>{description}</p>
         </div>
     )
 }
 
-
-// export function CardHeader({ title, img }: Omit<Props, "description">) { // Omit: remove description from Props type
-export function CardHeader({ title, img }: Pick<Props, "img" | "title">) { // Pick: only pick img and title from Props type
+export function CardHeader({ title, img }: Omit<Props, "description">) { // Omit: remove description from Props type
     return (
         <div className="card-header">
             <img src={img} alt={title} />
